1ed282e3126d16bb2b85770427eae9d300fd668f
vfvgfdvgf <halax.7y7@gmail.com>
2025-09-24 23:20:48 +0300

    urls.py

--- palmsite/urls.py
+++ palmsite/urls.py
@@ -1,19 +1,19 @@
-from main.admin import custom_admin_site
-from django.urls import path, include, re_path
+from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from main import views
-from django.shortcuts import redirect
+from main.admin import custom_admin_site
 
 urlpatterns = [
-    path('', views.home, name='home'),  # الصفحة الرئيسية
-    path('admin/', custom_admin_site.urls),
-    path('', include('main.urls')),  # توجيه كل الطلبات لتطبيق main
-    path('i18n/', include('django.conf.urls.i18n')),  # دعم تغيير اللغة
+    path("", views.home, name="home"),  # الصفحة الرئيسية
+    path("admin/", custom_admin_site.urls),
+    path("", include("main.urls")),  # روابط التطبيق
+    path("i18n/", include("django.conf.urls.i18n")),  # دعم تعدد اللغات
 ]
 
-# السماح بعرض الملفات الإعلامية أثناء التطوير
+# في وضع التطوير فقط
 if settings.DEBUG:
+    # عرض الملفات الإعلامية (media)
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
-
+    # عرض الملفات الثابتة (static)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
